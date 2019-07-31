8904294a725f90cc1ee44718783a73daea68f46e
felixdumont <53409417+felixdumont@users.noreply.github.com>
2019-07-31 09:51:36 -0400

    Cleanup

--- processing/business_utils.py
+++ processing/business_utils.py
@@ -1,8 +1,28 @@
 
-def calculate_distance(x, y):
+def calculate_distance(loc1, loc2):
+    """
+    Calculate the distance in miles between locations 1 and 2
+    :param loc1: Tuple containing coordinates for loc1
+    :param loc2: Tuple containing coordinates for loc2
+    :return: Distance in miles (float
+    """
     pass
 
 
-def walking_time(distance, speed):
+def walking_time(distance, speed=2):
+    """
+    Calculates the time required to walk the distance for a given speed.
+    :param distance: Distance in miles
+    :param speed: Speed in miles/hr (default value = 2 miles/hr)
+    :return: Walking time in minutes
+    """
     pass
 
+
+def generate_distance_matrix(locations):
+    """
+    Given a list of n tuples representing location coordinates, return an nxn matrix containing all of the distances
+    :param locations: list of n tuples representing location coordinates
+    :return: nxn matrix containing all of the distances
+    """
+    pass
